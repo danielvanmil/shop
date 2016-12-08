ff47e95a82db974a1009850aa9a76541e3b59da4
Daniel van Mil <Daniel.vanMil@gxsoftware.com>
2016-12-08 14:52:07 +0100

    converted category data element to TS

---
+++ src/shop-category-data.ts
@@ -0,0 +1,148 @@
+class ShopCategoryData {
+
+    properties: any;
+
+    categories: Array<any>;
+
+    categoryName: string;
+
+    categoryList: Array<any>;
+
+    is: 'shop-category-data';
+
+    beforeRegister() {
+      
+        this.categoryList = [
+            {
+                name: 'mens_outerwear',
+                title: 'Men\'s Outerwear',
+                image: '/images/mens_outerwear.jpg',
+                placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXAABAQEAAAAAAAAAAAAAAAAAAAIEAQEAAAAAAAAAAAAAAAAAAAACEAAAAwYHAQAAAAAAAAAAAAAAERMBAhIyYhQhkaEDIwUVNREBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A3dkr5e8tfpwuneJITOzIcmQpit037Bw4mnCVNOpAAQv/2Q=='
+            },
+            {
+                name: 'ladies_outerwear',
+                title: 'Ladies Outerwear',
+                image: '/images/ladies_outerwear.jpg',
+                placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWQABAQAAAAAAAAAAAAAAAAAAAAEBAQEAAAAAAAAAAAAAAAAAAAIDEAABAwMFAQAAAAAAAAAAAAARAAEygRIDIlITMwUVEQEBAAAAAAAAAAAAAAAAAAAAQf/aAAwDAQACEQMRAD8Avqn5meQ0kwk1UyclmLtNj7L4PQoioFf/2Q=='
+            },
+            {
+                name: 'mens_tshirts',
+                title: 'Men\'s T-Shirts',
+                image: '/images/mens_tshirts.jpg',
+                placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWwABAQEAAAAAAAAAAAAAAAAAAAMEAQEAAAAAAAAAAAAAAAAAAAAAEAABAwEJAAAAAAAAAAAAAAARAAESEyFhodEygjMUBREAAwAAAAAAAAAAAAAAAAAAAEFC/9oADAMBAAIRAxEAPwDb7kupZU1MTGnvOCgxpvzEXTyRElCmf//Z'
+            },
+            {
+                name: 'ladies_tshirts',
+                title: 'Ladies T-Shirts',
+                image: '/images/ladies_tshirts.jpg',
+                placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXwABAQEAAAAAAAAAAAAAAAAAAAMFAQEBAAAAAAAAAAAAAAAAAAABAhAAAQIDCQAAAAAAAAAAAAAAEQABITETYZECEjJCAzMVEQACAwAAAAAAAAAAAAAAAAAAATFBgf/aAAwDAQACEQMRAD8AzeADAZiFc5J7BC9Scek3VrtooilSNaf/2Q=='
+            }
+        ];
+
+        this.properties = {
+
+            categoryName: String,
+
+            itemName: String,
+
+            categories: {
+                type: Array,
+                value: this.categoryList,
+                readOnly: true,
+                notify: true
+            },
+
+            category: {
+                type: Object,
+                computed: '_computeCategory(categoryName)',
+                notify: true
+            },
+
+            item: {
+                type: Object,
+                computed: '_computeItem(category.items, itemName)',
+                notify: true
+            },
+
+            failure: {
+                type: Boolean,
+                notify: true,
+                readOnly: true
+            }
+        };
+
+        this.categories = this.categoryList;
+    };
+
+    _setFailure(state: boolean) {
+        console.log("FIXME");
+    };
+
+    _getCategoryObject(categoryName) {
+        for (var i = 0, c; c = this.categories[i]; ++i) {
+            if (c.name === categoryName) {
+                return c;
+            }
+        }
+        return null;
+    };
+
+    _computeCategory(categoryName) {
+        // Fetch the items of the category. Note that the fetch is asynchronous,
+        // which means `category.items` may not be set initially (but that path
+        // will be notified when the fetch completes).
+        var categoryObj = this._getCategoryObject(categoryName);
+        this._fetchItems(categoryObj, 1);
+        return categoryObj;
+    };
+
+    _computeItem(items, itemName) {
+        for (var i = 0, item; item = items[i]; ++i) {
+            if (item.name === itemName) {
+                return item;
+            }
+        }
+    };
+
+    _fetchItems(category, attempts) {
+        this._setFailure(false);
+        // Only fetch the items of a category if it has not been previously set.
+        if (!category || category.items) {
+            return;
+        }
+        this._getResource({
+            url: '/data/' + category.name + '.json',
+            onLoad(e) {
+                this.set('category.items', JSON.parse(e.target.responseText));
+            },
+            onError(e) {
+                this._setFailure(true);
+            }
+        }, attempts);
+    };
+
+    _getResource(rq, attempts) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', rq.onLoad.bind(this));
+        xhr.addEventListener('error', function(e) {
+            // Flaky connections might fail fetching resources
+            if (attempts > 1) {
+                this.debounce('_getResource', this._getResource.bind(this, rq, attempts - 1), 200);
+            } else {
+                rq.onError.call(this, e);
+            }
+        }.bind(this));
+
+        xhr.open('GET', rq.url);
+        xhr.send();
+    };
+
+    refresh() {
+        if (this.categoryName) {
+            // Try at most 3 times to get the items.
+            this._fetchItems(this._getCategoryObject(this.categoryName), 3);
+        }
+    }
+};
+
+Polymer(ShopCategoryData);
---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+interface Category {
+    name: string;
+    title: string;
+    image: string;
+    placeholder: string;
+}
+
+interface Article {
+    name: string;
+    title: string;
+    category: string;
+    price: number;
+    description: string,
+    image: string;
+    largeImage: string;
+}
